7741b3251e38f49fac0f4e290459dbf8e27148d0
unknown <philipboluwatife0@gmail.com>
2022-05-24 11:01:58 +0100

    ss

---
+++ blog/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+def handle_not_found(request,exception):
+    return render(request, 'not-found.html')
+
+def home(request):
+    return render(request, 'home.html', {})
