161b634caa0714e3b7ca08905ce3cbdbd5ae84b1
unknown <philipboluwatife0@gmail.com>
2022-06-02 10:22:33 +0100

    moving to another pc jumping just like a monkey@

--- members/views.py
+++ members/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth
 
 # Create your views here.
 def signup(request):
+
+    if request.method == "POST":
+        username = request.POST['username']
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        email = request.POST['email']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
+
+        myuser = User.objects.create(username, email, pass1)
+        myuser.first_name = fname
+        myuser.last_name = lname
+
+        myuser.save()
+
+
     return render(request, "registration/signup.html")
 
 def signin(request):
